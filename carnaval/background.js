a16e4c8e2353581c66d916672b27456d7ecbe556
Victor Martins <victorfrontend@gmail.com>
2016-12-10 16:20:31 -0300

    fiz notification days

--- carnaval/background.js
+++ carnaval/background.js
@@ -1,46 +1,44 @@
 // ------------------------------------------------------------------------------
 // COUNTDOWN --------------------------------------------------------------------
+var end = new Date(2017,01,23);
+
+var _second = 1000;
+var _minute = _second * 60;
+var _hour = _minute * 60;
+var _day = _hour * 24;
+var timer;
+var now = new Date();
+var distance = end - now;
+
+var date = {
+  days: Math.floor(distance / _day),
+  hours: Math.floor((distance % _day) / _hour),
+  minutes: Math.floor((distance % _hour) / _minute),
+  seconds: Math.floor((distance % _minute) / _second)
+}
+
 function showRemaining() {
-  var end = new Date(2017,01,23);
-
-  var _second = 1000;
-  var _minute = _second * 60;
-  var _hour = _minute * 60;
-  var _day = _hour * 24;
-  var timer;
-  var now = new Date();
-  var distance = end - now;
-
   if (distance < 0) {
     clearInterval(timer);
     console.log('Já é Carnaval!!!');
 
     return;
   }
-  var days = Math.floor(distance / _day);
-  var hours = Math.floor((distance % _day) / _hour);
-  var minutes = Math.floor((distance % _hour) / _minute);
-  var seconds = Math.floor((distance % _minute) / _second);
+
+  date;
 }
 
+
 timer = setInterval(showRemaining);
+
 // ------------------------------------------------------------------------------
 // NOTIFICATIONS ----------------------------------------------------------------
-document.addEventListener('DOMContentLoaded', function () {
-  if (!Notification) {
-    alert('Desktop notifications not available in your browser. Try Chromium.');
-    return;
-  }
-  if (Notification.permission !== "granted")
-    Notification.requestPermission();
-});
-
 var url = "https://victormartins.com.br/carnaval/";
 var notifyMe = function() {
   if (Notification.permission !== "granted")
     Notification.requestPermission();
   else {
-    var notification = new Notification('Faltam ' + days + ' dias para o carnaval!', {
+    var notification = new Notification('Faltam ' + date.days + ' dias para o carnaval!', {
       icon: 'icon128.png',
       body: "aaaaaaaaaaaa",
     });
@@ -51,9 +49,3 @@ var notifyMe = function() {
 }
 
 chrome.windows.onCreated.addListener(notifyMe);
-
-
-function cb() {
-  console.log(days)
-}
-setInterval(cb, 1000)
